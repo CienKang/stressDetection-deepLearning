b5393d710fc53bb77089690d73bbc1b8c194e0a9
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2022-11-08 12:56:21 +0530

    Pie Chart

--- src/components/AudioRecorder.js
+++ src/components/AudioRecorder.js
@@ -3,6 +3,7 @@ import { AiOutlineAudio, AiTwotoneAudio } from "react-icons/ai";
 import { BsFillSquareFill, BsFillCaretRightFill } from "react-icons/bs";
 import { RiSendPlaneFill } from "react-icons/ri";
 import { useEffect, useState } from "react";
+import AudioStressPieChart from "./graphComponents/AudioStressPieChart";
 
 const AudioRecorder = () => {
 
@@ -10,6 +11,13 @@ const AudioRecorder = () => {
 
     const [pulseEffect, setPulseEffect] = useState('');
     const [show, setShow] = useState(false);
+    const [netResult,setNetResult] = useState('Neutral');
+    const [pieData, setPieData] = useState([
+        { name: 'Probability of Stressed', value: 24 },
+        { name: 'Probability of Not Stressed', value: 76 }
+    ]);
+
+    const [showPie, setShowPie] = useState(false);
 
     useEffect(() => {
         if (isRecording === true)
@@ -24,16 +32,19 @@ const AudioRecorder = () => {
     }
 
     async function sendAudio() {
-        var data = new FormData()
-        var audio_blob = await getBlob(audioURL);
-        data.append('file', audio_blob, 'file')
-        fetch('http://localhost:5000/predict_voice', {
-            method: 'POST',
-            body: data
-        }).then(response => response.json()
-        ).then(json => {
-            console.log(json);
-        });
+        // var data = new FormData()
+        // var audio_blob = await getBlob(audioURL);
+        // data.append('file', audio_blob, 'file')
+        // fetch('http://localhost:5000/predict_voice', {
+        //     method: 'POST',
+        //     body: data
+        // }).then(response => response.json()
+        // ).then(json => {
+        //     console.log(json);
+                // setNetResult(json)
+        // });
+
+        setShowPie(true);
     }
 
     return (
@@ -41,7 +52,7 @@ const AudioRecorder = () => {
             <div className="text-center text-white">
                 <h1 className="display-1 ">Do you want to share with us how was your Day?</h1>
                 <p className="fs-3">This will help us strengthen our evaluation for better result.</p>
-                <button onClick={()=>{setShow(true)}} className="btn btn-success m-4">Yes , Go Ahead!!</button>
+                <button onClick={() => { setShow(true) }} className="btn btn-success m-4">Yes , Go Ahead!!</button>
 
                 {
                     show &&
@@ -68,8 +79,16 @@ const AudioRecorder = () => {
                                 <RiSendPlaneFill style={{ fontSize: '1.9rem' }} />
                             </button>
                         </div>
+
+                        {
+                            showPie &&
+                            <div className="d-flex align-items-center justify-content-center bg-primary ">
+                                <AudioStressPieChart data={pieData} />
+                            </div>
+                        }
                     </>
                 }
+
             </div>
         </div>
     );
